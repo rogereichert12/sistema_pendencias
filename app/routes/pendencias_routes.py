7480db316e5801dfc4b6bbcc4548464fcc402f73
Roger Reichert <rogereichert12@gmail.com>
2024-11-18 16:46:57 -0300

    Foi melhorado a função que fazia o cadastro de pendências

--- app/routes/pendencias_routes.py
+++ app/routes/pendencias_routes.py
@@ -143,3 +143,46 @@ def pagar_pendencia(pendencia_id):
     finally:
         cursor.close()
         close_db_connection(conn)
+
+
+@pendencias_blueprint.route("/add", methods=["POST"])
+def add_pendencia():
+    data = request.json
+    conn = get_db_connection(Config.DB_CONFIG)
+    cursor = conn.cursor()
+    try:
+        # Validação do formato da data
+        try:
+            data_formatada = datetime.strptime(data["data"], "%Y-%m-%d").strftime(
+                "%Y-%m-%d"
+            )
+        except ValueError:
+            return jsonify({"error": "Formato de data inválido. Use YYYY-MM-DD."}), 400
+
+        # Cálculo do valor total
+        valor_total = sum(
+            float(item["subtotal"]) for item in data["produtos"] if "subtotal" in item
+        )
+
+        # Insere a pendência
+        cursor.execute(
+            "INSERT INTO pendencias (cliente_id, data, valor) VALUES (%s, %s, %s)",
+            (data["cliente_id"], data_formatada, valor_total),
+        )
+        pendencia_id = cursor.lastrowid
+
+        # Insere os produtos da pendência
+        for item in data["produtos"]:
+            cursor.execute(
+                "INSERT INTO pendencias_produtos (pendencia_id, produto_id, quantidade, subtotal) VALUES (%s, %s, %s, %s)",
+                (pendencia_id, item["produtoId"], item["quantidade"], item["subtotal"]),
+            )
+
+        conn.commit()
+        return jsonify({"success": True})
+    except Exception as e:
+        print("Erro ao salvar pendência:", str(e))
+        return jsonify({"error": str(e)}), 500
+    finally:
+        cursor.close()
+        close_db_connection(conn)
